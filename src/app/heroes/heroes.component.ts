6f230508a194b9c26871ff00ff2c436b1fa7ca24
Gontran Bezerra <gontranbezerra@gmail.com>
2021-06-01 18:39:31 -0300

    feat: Navigating to hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 // import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+// import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,21 +13,21 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   // heroes = HEROES;
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
 
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService
+    // private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
   // getHeroes(): void {
   //   this.heroes = this.heroService.getHeroes();
