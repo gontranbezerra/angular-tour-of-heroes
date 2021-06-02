6adddede54680744f1a4565fd62bae9d7bbab8ca
Gontran Bezerra <gontranbezerra@gmail.com>
2021-06-01 21:09:09 -0300

    feat: Delete a hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,9 +16,8 @@ export class HeroesComponent implements OnInit {
   // selectedHero?: Hero;
 
   constructor(
-    private heroService: HeroService
-  ) // private messageService: MessageService
-  {}
+    private heroService: HeroService // private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -41,10 +40,15 @@ export class HeroesComponent implements OnInit {
     if (!name) {
       return;
     }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
     this.heroService
-        .addHero({ name } as Hero)
-        .subscribe((hero) => {
-          this.heroes.push(hero);
-        });
+        .deleteHero(hero.id)
+        .subscribe();
   }
 }
