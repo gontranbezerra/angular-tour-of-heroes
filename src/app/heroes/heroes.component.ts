683e4540bed10192b01ee6d42f2766de0149a7e9
Gontran Bezerra <gontranbezerra@gmail.com>
2021-06-01 21:01:05 -0300

    feat: Add a new hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,9 +16,9 @@ export class HeroesComponent implements OnInit {
   // selectedHero?: Hero;
 
   constructor(
-    private heroService: HeroService,
-    // private messageService: MessageService
-  ) {}
+    private heroService: HeroService
+  ) // private messageService: MessageService
+  {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -34,5 +34,17 @@ export class HeroesComponent implements OnInit {
   // }
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService
+        .addHero({ name } as Hero)
+        .subscribe((hero) => {
+          this.heroes.push(hero);
+        });
   }
 }
